566926cc04d0365be86ec8f3f8aee3a0bd30c400
Nimish Bongale <nimishnb98@gmail.com>
2019-10-08 16:11:57 +0530

    Final changes

--- App.js
+++ App.js
@@ -1,27 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
-import {
-  StyleSheet,
-  Text
-} from 'react-native';
+import React, {Fragment} from 'react'
+import Navigator from './Navigator'
 
 const App = () => {
   return (
-   <Text style="styles.text1">DocAid</Text>
+      <Navigator/>
   );
 };
-
-const styles = StyleSheet.create({
-    text1:{
-      fontWeight:'bold'
-    }
-});
 
 export default App;
